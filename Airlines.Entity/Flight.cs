213878c76023658fb8fdaf2be4b255e6b23e364a
ilkeresen <63172025+ilkeresen@users.noreply.github.com>
2020-06-05 21:14:07 +0300

    Flight Update

--- Airlines.Entity/Flight.cs
+++ Airlines.Entity/Flight.cs
@@ -21,7 +21,9 @@ namespace Airlines.Entity
         [Required]
         public decimal Price { get; set; }
 
-        public Plane PlaneId { get; set; }
+        public int? PlaneId { get; set; }
+
+        //public Plane Plane { get; set; }
 
         public int AirlineNumber { get; set; }
 
